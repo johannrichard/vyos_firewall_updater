282a4ea5124b95d8d738699c603645fad1784946
Brendan Ragan <lordmortis@gmail.com>
2020-07-12 17:56:12 +0800

    basic working system
    works!

--- update.py
+++ update.py
@@ -1,24 +1,26 @@
+#!/usr/bin/python
+
 import argparse
 import json
 import socket
 
 parser = argparse.ArgumentParser(description='Update firewall config')
-parser.add_argument('--config', metavar='config_file', type=file, default="config.json",
+parser.add_argument('--config', metavar='config_file', type=file, default="update_config.json",
                     help='path to the config file')
 
 args = parser.parse_args()
 config = json.load(args.config)
 if len(config) == 0:
-    print "No groups found in config"
     exit(-1)
 
 for groupName in config:
+    print "delete firewall group address-group %s address" % groupName
     ip_addresses = []
     for host in config[groupName]:
         ip = socket.gethostbyname(host)
         if not ip_addresses.__contains__(ip):
             ip_addresses.append(ip)
-    print ("Setting group %s to contain: " % groupName)
     for ip in ip_addresses:
-        print(ip)
+        print "set firewall group address-group %s address '%s'" %(groupName, ip)
+    print "commit"
 

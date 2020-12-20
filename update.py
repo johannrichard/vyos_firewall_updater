920e57f3f045f35ec84aa748d67b7893d00bfea9
Johann Richard <189003+johannrichard@users.noreply.github.com>
2020-12-20 18:10:31 +0100

    fix(update): catch unresolved hostnames
    - add a try: / except: block to handle unresolved hostnames
    - should make the update more robust for certain edge-cases

--- update.py
+++ update.py
@@ -17,9 +17,12 @@ for groupName in config:
     print "delete firewall group address-group %s address" % groupName
     ip_addresses = []
     for host in config[groupName]:
-        ip = socket.gethostbyname(host)
-        if not ip_addresses.__contains__(ip):
-            ip_addresses.append(ip)
+        try:
+            ip = socket.gethostbyname(host)
+            if not ip_addresses.__contains__(ip):
+                ip_addresses.append(ip)
+        except:
+            pass
     for ip in ip_addresses:
         print "set firewall group address-group %s address '%s'" %(groupName, ip)
     print "commit"
